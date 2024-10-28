c17a72be91d5a479b06723a8bfcd7da1a2e0db7b
MekuMaki <sage2014@gmail.com>
2024-10-27 22:43:59 -0500

    Update mektools_support_community_panel.py
    oops lol, now this should be right

--- panels/mektools_support_community_panel.py
+++ panels/mektools_support_community_panel.py
@@ -2,7 +2,7 @@ import bpy
 from bpy.types import Panel
 
 class VIEW3D_PT_SupportCommunity(Panel):
-    bl_label = "MekTools 1.0.0"  # Manually set version here
+    bl_label = "MekTools 1.0.1" 
     bl_idname = "VIEW3D_PT_support_community"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
