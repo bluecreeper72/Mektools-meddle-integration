87a6e37461ad7348544bc9ecb096a6e10f9607f6
MekuMaki <sage2014@gmail.com>
2024-10-30 03:10:23 -0500

    Update mektools_import_panel.py
    removing the Misc panel... this should just be a hidden panel

--- panels/mektools_import_panel.py
+++ panels/mektools_import_panel.py
@@ -26,8 +26,54 @@ class VIEW3D_PT_ImportPanel(Panel):
         split.popover("MEKTOOLS_PT_MaleRigs", text="Male", icon_value=0)
         split.popover("MEKTOOLS_PT_FemaleRigs", text="Female", icon_value=0)
 
+class MEKTOOLS_PT_MaleRigs(Panel):
+    """Male Mekrigs Import"""
+    bl_label = "Male Mekrigs"
+    bl_idname = "MEKTOOLS_PT_MaleRigs"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Mektools"
+    bl_options = {'HIDE_HEADER'}
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("mektools.import_mekrig_midlander_male", text="Midlander Male")
+        layout.operator("mektools.import_mekrig_highlander_male", text="Highlander Male")
+        layout.operator("mektools.import_mekrig_elezen_male", text="Elezen Male")
+        layout.operator("mektools.import_mekrig_miqote_male", text="Miqote Male")
+        layout.operator("mektools.import_mekrig_roegadyn_male", text="Roegadyn Male")
+        layout.operator("mektools.import_mekrig_lalafell_both", text="Lalafell Male")
+        layout.operator("mektools.import_mekrig_aura_male", text="Aura Male")
+        layout.operator("mektools.import_mekrig_hrothgar_male", text="Hrothgar Male")
+        layout.operator("mektools.import_mekrig_viera_male", text="Viera Male")
+
+class MEKTOOLS_PT_FemaleRigs(Panel):
+    """Female Mekrigs Import"""
+    bl_label = "Female Mekrigs"
+    bl_idname = "MEKTOOLS_PT_FemaleRigs"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Mektools"
+    bl_options = {'HIDE_HEADER'}
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("mektools.import_mekrig_midlander_female", text="Midlander Female")
+        layout.operator("mektools.import_mekrig_highlander_female", text="Highlander Female")
+        layout.operator("mektools.import_mekrig_elezen_female", text="Elezen Female")
+        layout.operator("mektools.import_mekrig_miqote_female", text="Miqote Female")
+        layout.operator("mektools.import_mekrig_roegadyn_female", text="Roegadyn Female")
+        layout.operator("mektools.import_mekrig_lalafell_both", text="Lalafell Female")
+        layout.operator("mektools.import_mekrig_aura_female", text="Aura Female")
+        layout.operator("mektools.import_mekrig_hrothgar_female", text="Hrothgar Female")
+        layout.operator("mektools.import_mekrig_viera_female", text="Viera Female")
+
 def register():
     bpy.utils.register_class(VIEW3D_PT_ImportPanel)
+    bpy.utils.register_class(MEKTOOLS_PT_MaleRigs)
+    bpy.utils.register_class(MEKTOOLS_PT_FemaleRigs)
 
 def unregister():
     bpy.utils.unregister_class(VIEW3D_PT_ImportPanel)
+    bpy.utils.unregister_class(MEKTOOLS_PT_MaleRigs)
+    bpy.utils.unregister_class(MEKTOOLS_PT_FemaleRigs)
